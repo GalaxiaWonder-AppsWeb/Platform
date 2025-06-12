5339489bf8934c08616e7ccc131acbd28346aa5d
Henry Reaño <henry29.2004@hotmail.com>
2025-06-11 21:45:03 -0500

    feat(ep15): add person query service interface in iam context

---
+++ Platform.API/IAM/Domain/Services/IPersonQueryService.cs
@@ -0,0 +1,9 @@
+﻿using Platform.API.IAM.Domain.Model.Aggregates;
+using Platform.API.IAM.Domain.Model.Queries;
+
+namespace Platform.API.IAM.Domain.Services;
+
+public interface IPersonQueryService
+{
+    Task<IEnumerable<Person>> Handle(GetAllPersonsQuery query);
+}
