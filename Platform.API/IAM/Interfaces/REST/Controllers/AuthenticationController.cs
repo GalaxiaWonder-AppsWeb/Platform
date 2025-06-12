feb4e65815e50979133397c406ad41d2f868a568
Henry Reaño <henry29.2004@hotmail.com>
2025-06-12 01:37:28 -0500

    feat(ep15): add sign in endpoint

---
+++ Platform.API/IAM/Interfaces/REST/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+using Platform.API.IAM.Domain.Services;
+using Platform.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using Platform.API.IAM.Interfaces.REST.Assemblers;
+using Platform.API.IAM.Interfaces.REST.Resources;
+
+namespace Platform.API.IAM.Interfaces.REST.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Authentication endpoints")]
+public class AuthenticationController(IUserAccountCommandService userAccountCommandService) : ControllerBase 
+{
+    [HttpPost("sign-in")]
+    [AllowAnonymous]
+    [SwaggerOperation(
+        Summary = "Sign in",
+        Description = "Sign in a user",
+        OperationId = "SignIn")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The user was authenticated", typeof(AuthenticatedUserAccountResource))]
+    public async Task<IActionResult> SignIn([FromBody] SignInResource signInResource)
+    {
+        var signInCommand = SignInCommandFromResourceAssembler.ToCommandFromResource(signInResource);
+        var authenticatedUser = await userAccountCommandService.Handle(signInCommand);
+        var resource =
+            AuthenticatedUserResourceFromEntityAssembler.ToResourceFromEntity(authenticatedUser.userAccount,
+                authenticatedUser.token);
+        return Ok(resource);
+    }
+}
