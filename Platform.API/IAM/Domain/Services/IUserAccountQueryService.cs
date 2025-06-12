fa620273c3c958dc2c1d723dc6df1e5b761aa30b
Henry Reaño <henry29.2004@hotmail.com>
2025-06-11 23:05:12 -0500

    feat(ep15): add jwt service setup

---
+++ Platform.API/IAM/Domain/Services/IUserAccountQueryService.cs
@@ -0,0 +1,9 @@
+﻿using Platform.API.IAM.Domain.Model.Aggregates;
+using Platform.API.IAM.Domain.Model.Queries;
+
+namespace Platform.API.IAM.Domain.Services;
+
+public interface IUserAccountQueryService
+{
+    Task<UserAccount?> Handle(GetUserAccountByIdQuery query);
+}
