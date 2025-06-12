99a01bb306039d692f26db3439ea8a40add2bb46
Henry Reaño <henry29.2004@hotmail.com>
2025-06-12 00:55:14 -0500

    feat(ep15): add user account command and query service implementation

---
+++ Platform.API/IAM/Application/Internal/CommandServices/UserAccountCommandService.cs
@@ -0,0 +1,50 @@
+﻿using Platform.API.IAM.Application.Internal.OutboundServices;
+using Platform.API.IAM.Domain.Model.Aggregates;
+using Platform.API.IAM.Domain.Model.Commands;
+using Platform.API.IAM.Domain.Model.ValueObjects;
+using Platform.API.IAM.Domain.Repositories;
+using Platform.API.IAM.Domain.Services;
+using Platform.API.IAM.Infrastructure.Hashing.BCrypt.Services;
+using Platform.API.Shared.Domain.Repositories;
+
+namespace Platform.API.IAM.Application.Internal.CommandServices;
+
+public class UserAccountCommandService(
+    IUserAccountRepository userAccountRepository,
+    IPersonRepository personRepository,
+    ITokenService tokenService,
+    IHashingService hashingService,
+    IUnitOfWork unitOfWork) 
+    : IUserAccountCommandService
+{
+    public async Task<(UserAccount userAccount, string token)> Handle(SignInCommand command)
+    {
+        var userAccount = await userAccountRepository.FindByEmailAsync(command.Email);
+        if (userAccount == null || !hashingService.VerifyPassword(command.Password, userAccount.PasswordHash.ToString()))
+            throw new Exception("Invalid email or password");
+        var token = tokenService.GenerateToken(userAccount);
+        
+        return (userAccount, token);
+    }
+
+    public async Task Handle(SignUpCommand command)
+    {
+        if (userAccountRepository.ExistsByUserName(new UserName(command.Username)))
+            throw new Exception("Username {command.Username} is already taken");
+        if (personRepository.ExistsByEmail(new EmailAddress(command.Email)))
+            throw new Exception("Email {command.Email} is already taken");
+
+        var hashedPassword = hashingService.HashPassword(command.Password);
+        var userAccount = new UserAccount(command.Username, hashedPassword);
+
+        try
+        {
+            await userAccountRepository.AddAsync(userAccount);
+            await unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"An error occurred while creating User Account: {e.Message} ");
+        }
+    }
+}
