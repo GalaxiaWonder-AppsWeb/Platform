261a4c4a3d61a14abf6255f3674abb9ee9fd37be
Henry Reaño <henry29.2004@hotmail.com>
2025-06-11 21:53:47 -0500

    feat(ep15): add user type command and query service in iam context

---
+++ Platform.API/IAM/Domain/Services/IUserTypeCommandService.cs
@@ -0,0 +1,8 @@
+﻿using Platform.API.IAM.Domain.Model.Commands;
+
+namespace Platform.API.IAM.Domain.Services;
+
+public interface IUserTypeCommandService
+{
+    void Handle(SeedUserTypeCommand command);
+}
---
+++ Platform.API/IAM/Domain/Services/IUserTypeQueryService.cs
@@ -0,0 +1,9 @@
+﻿using Platform.API.IAM.Domain.Model.Queries;
+using Platform.API.IAM.Domain.Model.ValueObjects;
+
+namespace Platform.API.IAM.Domain.Services;
+
+public interface IUserTypeQueryService
+{
+    Task<IEnumerable<UserTypes>> Handle(GetAllUserTypesQuery query);
+}
