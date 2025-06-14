0e0c6c679bacda16e52e55020878ae65c8097ded
Henry Reaño <henry29.2004@hotmail.com>
2025-06-13 21:22:53 -0500

    feat(ep18): add iam context facade

---
+++ Platform.API/IAM/Application/ACL/IAMContextFacade.cs
@@ -0,0 +1,43 @@
+﻿using Platform.API.IAM.Domain.Repositories;
+using Platform.API.IAM.Infrastructure.Persistence.EFC.Repositories;
+using Platform.API.IAM.Interfaces.ACL;
+using Platform.API.Shared.Domain.Repositories.Model.ValueObjects;
+
+namespace Platform.API.IAM.Application.ACL;
+
+public class IAMContextFacade(IPersonRepository personRepository) : IIAMContextFacade
+{
+    public async Task<ProfileDetails?> GetProfileDetailsByIdAsync(long personId)
+    {
+        var person = await personRepository.FindByIdAsync(personId);
+        if (person is null) return null;
+
+        return new ProfileDetails(
+            person.Id,
+            person.Name.FirstName,
+            person.Name.LastName,
+            person.Email.Address
+        );
+    }
+
+    public async Task<List<ProfileDetails>> GetProfileDetailsByIdsAsync(IEnumerable<long> personIds)
+    {
+        var result = new List<ProfileDetails>();
+
+        foreach (var id in personIds)
+        {
+            var person = await personRepository.FindByIdAsync(id);
+            if (person != null)
+            {
+                result.Add(new ProfileDetails(
+                    person.Id,
+                    person.Name.FirstName,
+                    person.Name.LastName,
+                    person.Email.Address
+                ));
+            }
+        }
+
+        return result;
+    }
+}
