6db7d41c1db6898c41ab6ce7ee8891ace4fa82a0
Henry Reaño <henry29.2004@hotmail.com>
2025-06-15 17:49:16 -0500

    feat(ep09): add organization interface and implementation repository

---
+++ Platform.API/Organizations/Domain/Repositories/IOrganizationRepository.cs
@@ -0,0 +1,17 @@
+﻿using Platform.API.Organizations.Domain.Model.Aggregates;
+using Platform.API.Shared.Domain.Repositories;
+
+namespace Platform.API.Organizations.Domain.Repositories;
+
+public interface IOrganizationRepository : IBaseRepository<Organization>
+{
+    bool ExistsByRuc(string ruc);
+    
+    Task<Organization?> FindByRucAsync(string ruc);
+
+    Task<Organization?> FindOrganizationByMemberId(long memberId);
+    
+    Task<Organization?> FindOrganizationByInvitationId(long invitationId);
+    
+    Task<IEnumerable<Organization>> FindOrganizationsByOrganizationMemberPersonId(long memberPersonId);
+}
---
+++ Platform.API/Organizations/Infrastructure/Persistence/EFC/Repositories/OrganizationRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Platform.API.Organizations.Domain.Model.Aggregates;
+using Platform.API.Organizations.Domain.Model.Entities;
+using Platform.API.Organizations.Domain.Repositories;
+using Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace Platform.API.Organizations.Infrastructure.Persistence.EFC.Repositories;
+
+public class OrganizationRepository(AppDbContext context) : BaseRepository<Organization>(context), IOrganizationRepository
+{
+    public bool ExistsByRuc(string ruc)
+    {
+        return Context.Set<Organization>().Any(organization => organization.Ruc.Number.Equals(ruc));
+    }
+
+    public async Task<Organization?> FindByRucAsync(string ruc)
+    {
+        return await Context.Set<Organization>()
+            .FirstOrDefaultAsync(organization => organization.Ruc.Number.Equals(ruc));
+    }
+
+    public async Task<Organization?> FindOrganizationByMemberId(long memberId)
+    {
+        var organizationId = await Context.Set<OrganizationMember>()
+            .Where(om => om.Id.Equals(memberId))
+            .Select(om => om.OrganizationId.organizationId)
+            .FirstOrDefaultAsync();
+
+        if (organizationId.Equals(0)) return null;
+        
+        return await Context.Set<Organization>().FindAsync(organizationId);
+    }
+
+    public async Task<Organization?> FindOrganizationByInvitationId(long invitationId)
+    {
+        var organizationId = await Context.Set<OrganizationInvitation>()
+            .Where(oi => oi.Id.Equals(invitationId))
+            .Select(oi => oi.OrganizationId.organizationId)
+            .FirstOrDefaultAsync();
+        
+        if (organizationId.Equals(0)) return null;
+        
+        return await Context.Set<Organization>().FindAsync(organizationId);
+    }
+
+    public async Task<IEnumerable<Organization>> FindOrganizationsByOrganizationMemberPersonId(long memberPersonId)
+    {
+        var organizationIds = await Context.Set<OrganizationMember>()
+            .Where(om => om.PersonId.Equals(memberPersonId))
+            .Select(om => om.OrganizationId.organizationId)
+            .ToListAsync();
+
+        return await Context.Set<Organization>()
+            .Where(o => organizationIds.Contains(o.Id))
+            .ToListAsync();
+    }
+}
