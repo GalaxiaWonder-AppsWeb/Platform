30b8159daf23e2fe8a02d7464e5a4f5e0b8efae5
Henry Reaño <henry29.2004@hotmail.com>
2025-06-19 18:47:18 -0500

    feat(tb2): change program cs to use swagger

--- Platform.API/Program.cs
+++ Platform.API/Program.cs
@@ -151,11 +151,13 @@ using (var scope = app.Services.CreateScope())
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PropGMS.Platform.API v1");
+    c.RoutePrefix = "swagger";
+});
+
 
 app.UseCors("AllowAllPolicy");
 
