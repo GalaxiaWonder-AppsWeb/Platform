4a6e85c96e34156ee7229b3d803e3895ba9fba98
Henry Reaño <henry29.2004@hotmail.com>
2025-06-28 18:58:24 -0500

    feat(ep02): add project team member and specialty repositories

---
+++ Platform.API/Projects/Infrastructure/Persistence/EFC/Repositories/ProjectTeamMemberRepository.cs
@@ -0,0 +1,12 @@
+﻿using Platform.API.Projects.Domain.Model.Aggregates;
+using Platform.API.Projects.Domain.Repositories;
+using Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace Platform.API.Projects.Infrastructure.Persistence.EFC.Repositories;
+
+/// <inheritdoc cref="IProjectTeamMemberRepository"/>
+public class ProjectTeamMemberRepository(AppDbContext context): BaseRepository<ProjectTeamMember>(context), IProjectTeamMemberRepository
+{
+    
+}
