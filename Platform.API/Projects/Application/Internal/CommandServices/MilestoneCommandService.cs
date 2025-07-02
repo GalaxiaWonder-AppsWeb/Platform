315ff1ce9c01eb306a51a7bee0411825161b01c1
Henry Reaño <henry29.2004@hotmail.com>
2025-07-01 22:00:14 -0500

    feat(ep03): add milestone command and query implementation with all features

---
+++ Platform.API/Projects/Application/Internal/CommandServices/MilestoneCommandService.cs
@@ -0,0 +1,96 @@
+﻿using Platform.API.Projects.Domain.Model.Aggregates;
+using Platform.API.Projects.Domain.Model.Commands;
+using Platform.API.Projects.Domain.Repositories;
+using Platform.API.Projects.Domain.Services;
+using Platform.API.Shared.Domain.Repositories;
+
+namespace Platform.API.Projects.Application.Internal.CommandServices;
+
+public class MilestoneCommandService(
+    IMilestoneRepository milestoneRepository,
+    IProjectRepository projectRepository,
+    IUnitOfWork unitOfWork) : IMilestoneCommandService
+{
+    /// <summary>
+    /// Handles the creation of a new milestone.
+    /// </summary>
+    /// <param name="command">
+    /// The command containing the details to create the milestone, including the project ID, name, and description.
+    /// </param>
+    /// <returns>
+    /// The newly created <see cref="Milestone"/> entity, or null if the creation failed.
+    /// </returns>
+    public async Task<Milestone?> Handle(CreateMilestoneCommand command)
+    {
+        var project = await projectRepository.FindById(command.ProjectId.Value);
+        if (project is null)
+        {
+            throw new Exception($"Project {command.ProjectId.Value} not found");
+        }
+
+        var milestone = new Milestone(command);
+        await milestoneRepository.AddAsync(milestone);
+        await unitOfWork.CompleteAsync();
+        return milestone;
+    }
+    
+    /// <summary>
+    /// Handles the update of the milestone name.
+    /// </summary>
+    /// <param name="command">
+    /// The command containing the milestone ID and the new name for the milestone.
+    /// </param>
+    /// <returns>
+    /// The updated <see cref="Milestone"/> entity, or null if the update failed.
+    /// </returns>
+    public async Task<Milestone?> Handle(UpdateMilestoneNameCommand command)
+    {
+        var milestone = await milestoneRepository.FindById(command.Id);
+        if (milestone is null) throw new Exception($"Milestone with id {command.Id} not found");
+        
+        milestone.ReassignName(command.MilestoneName);
+        milestoneRepository.Update(milestone);
+        await unitOfWork.CompleteAsync();
+        return milestone;
+    }
+
+    /// <summary>
+    /// Handles the update of the milestone description.
+    /// </summary>
+    /// <param name="command">
+    /// The command containing the milestone ID and the new description for the milestone.
+    /// </param>
+    /// <returns>
+    /// The updated <see cref="Milestone"/> entity, or null if the update failed.
+    /// </returns>
+    public async Task<Milestone?> Handle(UpdateMilestoneDescriptionCommand command)
+    {
+        var milestone = await milestoneRepository.FindById(command.Id);
+        if (milestone is null) throw new Exception($"Milestone with id {command.Id} not found");
+        
+        milestone.ReassignDescription(command.MilestoneDescription);
+        milestoneRepository.Update(milestone);
+        await unitOfWork.CompleteAsync();
+        return milestone;
+    }
+    
+    /// <summary>
+    /// Handles the update of the milestone date range.
+    /// </summary>
+    /// <param name="command">
+    /// The command containing the milestone ID and the new start and end dates for the milestone.
+    /// </param>
+    /// <returns>
+    /// The updated <see cref="Milestone"/> entity, or null if the update failed.
+    /// </returns>
+    public async Task<Milestone?> Handle(UpdateMilestoneDateRangeCommand command)
+    {
+        var milestone = await milestoneRepository.FindById(command.Id);
+        if (milestone is null) throw new Exception($"Milestone with id {command.Id} not found");
+        
+        milestone.ReassignDateRange(command.DateRange);
+        milestoneRepository.Update(milestone);
+        await unitOfWork.CompleteAsync();
+        return milestone;
+    }
+}
