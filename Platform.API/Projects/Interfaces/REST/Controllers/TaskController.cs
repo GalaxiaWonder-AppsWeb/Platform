f527e41f4aa173a5bb3cb4b59a5785908802f800
Henry Reaño <henry29.2004@hotmail.com>
2025-07-02 19:05:06 -0500

    feat(ep04): add get tasks by milestone and tasks by person endpoints

--- Platform.API/Projects/Interfaces/REST/Controllers/TaskController.cs
+++ Platform.API/Projects/Interfaces/REST/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using Platform.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using Platform.API.Projects.Domain.Model.Queries;
 using Platform.API.Projects.Domain.Services;
 using Platform.API.Projects.Interfaces.REST.Assemblers;
 using Platform.API.Projects.Interfaces.REST.Resources;
@@ -56,5 +57,47 @@ public class TaskController(
         var response = await TaskResourceFromEntityAssembler.ToResourceFromEntity(task);
         
         return Ok(response);
+    }
+    
+    [HttpGet("by-milestone-id/{milestoneId}")]
+    [SwaggerOperation(
+        Summary = "Get Tasks by Milestone ID",
+        Description = "Retrieve all tasks associated with a specific milestone",
+        OperationId = "task-get-by-milestone-id")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Tasks retrieved successfully")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Tasks retrieval failed")]
+    public async Task<IActionResult> GetAllTasksByMilestoneId(long milestoneId)
+    {
+        var query = new GetAllTasksByMilestoneIdQuery(milestoneId);
+        var tasks = await taskQueryService.Handle(query);
+        var resources = new List<TaskResource>();
+        foreach (var task in tasks)
+        {
+            var resource = await TaskResourceFromEntityAssembler.ToResourceFromEntity(task);
+            resources.Add(resource);
+        }
+        
+        return Ok(resources);
+    }
+    
+    [HttpGet("by-person-id-and-milestone-id/{personId}/{milestoneId}")] //esto no tendria que llamarse asi, es un antipatron.
+    [SwaggerOperation(
+        Summary = "Get Tasks by Person ID and Milestone ID",
+        Description = "Retrieve all tasks associated with a specific person and milestone",
+        OperationId = "task-get-by-person-id-and-milestone-id")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Tasks retrieved successfully")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Tasks retrieval failed")]
+    public async Task<IActionResult> GetAllTasksByPersonIdAndMilestoneId(long personId, long milestoneId)
+    {
+        var query = new GetAllTasksByPersonIdAndMilestoneIdQuery(personId, milestoneId);
+        var tasks = await taskQueryService.Handle(query);
+        var resources = new List<TaskResource>();
+        foreach (var task in tasks)
+        {
+            var resource = await TaskResourceFromEntityAssembler.ToResourceFromEntity(task);
+            resources.Add(resource);
+        }
+        
+        return Ok(resources);
     }
 }
