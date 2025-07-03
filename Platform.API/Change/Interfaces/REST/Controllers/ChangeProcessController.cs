ab3860e3871174b7526d469c69a048ff27cb1178
Henry Reaño <henry29.2004@hotmail.com>
2025-07-03 17:06:38 -0500

    feat(ep07): add create change process endpoint

---
+++ Platform.API/Change/Interfaces/REST/Controllers/ChangeProcessController.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Platform.API.Change.Domain.Services;
+using Platform.API.Change.Interfaces.REST.Assemblers;
+using Platform.API.Change.Interfaces.REST.Resources;
+using Platform.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Platform.API.Change.Interfaces.REST.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available Change Process endpoints")]
+public class ChangeProcessController(
+    IChangeProcessQueryService changeProcessQueryService,
+    IChangeProcessCommandService changeProcessCommandService) : ControllerBase
+{
+    [HttpPost("by-project-id/{projectId}")]
+    [SwaggerOperation(
+        Summary = "Create a Change Process",
+        Description = "Create a new change process for a project",
+        OperationId = "change-process-create")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Change process created successfully", typeof(ChangeProcessResource))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Change process creation failed")]
+    public async Task<IActionResult> CreateChangeProcess(
+        long projectId, CreateChangeProcessResource resource)
+    {
+        var command = CreateChangeProcessComandFromResourceAssembler.ToCommandFromResource(projectId, resource);
+        var changeProcess = await changeProcessCommandService.Handle(command);
+        if (changeProcess is null)
+        {
+            return BadRequest("Change process creation failed.");
+        }
+        var response = await ChangeProcessResourceFromEntityAssembler.ToResourceFromEntity(changeProcess);
+        return Ok(response);
+    }
+}
---
+++ Platform.API/Change/Interfaces/REST/Resources/ChangeProcessResource.cs
@@ -0,0 +1,3 @@
+﻿namespace Platform.API.Change.Interfaces.REST.Resources;
+
+public record ChangeProcessResource(long Id, string Origin, string Status, string Justification, string? Response, long ProjectId);
