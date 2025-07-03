385c2a406d9103fb35ca5ca8c72236b863b73502
Henry Reaño <henry29.2004@hotmail.com>
2025-07-03 15:48:37 -0500

    feat(ep07): add repository queries to support command and queries services in change context

--- Platform.API/Change/Domain/Repositories/IChangeProcessStatusRepository.cs
+++ Platform.API/Change/Domain/Repositories/IChangeProcessStatusRepository.cs
@@ -1,9 +1,19 @@
 ﻿using Platform.API.Change.Domain.Model.Entities;
+using Platform.API.Change.Domain.Model.ValueObjects;
 using Platform.API.Shared.Domain.Repositories;
 
 namespace Platform.API.Change.Domain.Repositories;
 
 public interface IChangeProcessStatusRepository : IBaseRepository<ChangeProcessStatus>
 {
+    /// <summary>
+    /// Find an <see cref="ChangeProcessStatus"/> by its name.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the status as a <see cref="ChangeProcessStatuses"/>.
+    /// </param>
+    /// <returns>
+    /// The matching <see cref="ChangeProcessStatus"/> if found; otherwise, <c>null</c>.
+    /// </returns>
     Task<ChangeProcessStatus?> FindByName(string name);
 }
---
+++ Platform.API/Change/Infrastructure/Persistence/EFC/Repositories/ChangeOriginRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Platform.API.Change.Domain.Model.Entities;
+using Platform.API.Change.Domain.Repositories;
+using Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace Platform.API.Change.Infrastructure.Persistence.EFC.Repositories;
+
+public class ChangeOriginRepository(AppDbContext context) : BaseRepository<ChangeOrigin>(context), IChangeOriginRepository
+{
+    /// <summary>
+    /// Find an <see cref="ChangeOrigin"/> by its name.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the status as a string.
+    /// </param>
+    /// <returns>
+    /// The matching <see cref="ChangeOrigin"/> if found; otherwise, <c>null</c>.
+    /// </returns>
+    public async Task<ChangeOrigin?> FindByName(string name)
+    {
+        return await Context.Set<ChangeOrigin>()
+            .FirstOrDefaultAsync(co => co.Name.ToString() == name);
+    }
+}
