c1a6c984ca24f6f311027f6e1295c70cbb3576b2
Henry Reaño <henry29.2004@hotmail.com>
2025-07-03 19:06:02 -0500

    feat(ep19): add get total task budget amount by project id  endpoint

---
+++ Platform.API/Billings/Interfaces/REST/Controllers/TaskBudgetController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Platform.API.Billings.Domain.Model.Queries;
+using Platform.API.Billings.Domain.Services;
+using Platform.API.Billings.Interfaces.REST.Assemblers;
+using Platform.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Platform.API.Billings.Interfaces.REST.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available Task Budget endpoints")]
+public class TaskBudgetController(
+    ITaskBudgetQueryService taskBudgetQueryService) : ControllerBase
+{
+    
+    [HttpGet("projects/{projectId}/total-task-budget")]
+    public async Task<IActionResult> GetTotalTaskBudget(long projectId)
+    {
+        var query = new GetTotalTasksBudgetByProjectIdQuery(projectId);
+        var total = await taskBudgetQueryService.Handle(query);
+        var resource = MoneyResourceFromEntityAssembler.ToResourceFromEntity(total);
+        return Ok(resource);
+    }
+}
